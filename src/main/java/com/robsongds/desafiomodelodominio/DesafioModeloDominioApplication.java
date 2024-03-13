e690af6f07cee1d2a60a3dafda2399eeeae5da01
Robson <robson.080490@gmail.com>
2024-03-13 13:42:31 -0300

    first commit

---
+++ src/main/java/com/robsongds/desafiomodelodominio/DesafioModeloDominioApplication.java
@@ -0,0 +1,13 @@
+package com.robsongds.desafiomodelodominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModeloDominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModeloDominioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robsongds/desafiomodelodominio/DesafioModeloDominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robsongds.desafiomodelodominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
